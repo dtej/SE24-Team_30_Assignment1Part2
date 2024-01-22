db8b625146dcdd17bb04887f0cbd16d227038d0b
HariOm3# <chandrasekar.s@students.iiit.ac.in>
2024-01-22 17:29:17 +0530

    Added Trip and Support related files

---
+++ SmartVehicleBookingSystem/src/manager/TripFeedbackManager.java
@@ -0,0 +1,18 @@
+package manager;
+
+import dbobjects.TripFeedback;
+import dbobjects.User;
+
+public class TripFeedbackManager {
+
+	// For collecting the feedback from the user once they enter it
+	public void collectRatingFeedback(User user, TripFeedback tripFeedback) {
+		// To Do
+	}
+
+	// For displaying the menu of trip rating and feedback once trip ends
+	public TripFeedback displayRatingFeedback(User user) {
+		// To Do
+		return null;
+	}
+}
