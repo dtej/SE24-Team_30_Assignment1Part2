20e4ae9ca6d64005017bd516adb3df60a8cf91a0
Sudarshan Srinivasan <sudarshan.srinivasan@research.iiit.ac.in>
2024-01-22 18:58:18 +0530

    Introducing Vehicle Management

--- SmartVehicleBookingSystem/src/dbobjects/SmartVehicle.java
+++ SmartVehicleBookingSystem/src/dbobjects/SmartVehicle.java
@@ -1,5 +1,36 @@
 package dbobjects;
 
+import SmartVehicleBookingSystem.src.dbobjects.VehicleStatus;
+
+import java.util.Date;
+
 public class SmartVehicle {
+    private VehicleStatus status;
+    private float distanceTravelled;
+    private Date usageTime;
+
+    public VehicleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(VehicleStatus status) {
+        this.status = status;
+    }
+
+    public float getDistanceTravelled() {
+        return distanceTravelled;
+    }
+
+    public void setDistanceTravelled(float distanceTravelled) {
+        this.distanceTravelled = distanceTravelled;
+    }
+
+    public Date getUsageTime() {
+        return usageTime;
+    }
+
+    public void setUsageTime(Date usageTime) {
+        this.usageTime = usageTime;
+    }
 
 }
---
+++ SmartVehicleBookingSystem/src/dbobjects/VehicleState.java
@@ -0,0 +1,8 @@
+package SmartVehicleBookingSystem.src.dbobjects;
+
+public enum VehicleState {
+    AVAILABLE,
+    RESERVED,
+    RENEWED,
+    UNDER_REPAIR
+}
